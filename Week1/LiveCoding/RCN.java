b445b0a21c0b70e7a09d838a8ab4f28fe5cf9b06
BrendanMoses <brendanmosesjohn@gmail.com>
2026-09-26 00:02:53 +0530

    [Brendan] added: Created Repeat Customer Name Program

---
+++ Week1/LiveCoding/RCN.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class RCN {
+    static String reverseCustomerName(String name) {
+        String reverse = "";
+        for (int i = name.length() - 1; i >= 0; i--) {
+            reverse = reverse + name.charAt(i);
+        }
+        return reverse;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+        System.out.println("Original Name: " + name);
+        System.out.println("Reversed Name: " +
+                reverseCustomerName(name));
+    }
+}
